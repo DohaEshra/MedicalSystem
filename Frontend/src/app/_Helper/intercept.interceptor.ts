b281455cb2ec8daac6a19f75cbea83eeb1a303fd
marina <marinamagdy212121@gmail.com>
2022-06-16 18:56:32 +0200

    interceptor

---
+++ Frontend/src/app/_Helper/intercept.interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {AccountService} from '../_services/account.service'
+
+const TOKEN_HEADER_KEY = 'Authorization';       // for back-end
+
+@Injectable()
+export class InterceptInterceptor implements HttpInterceptor {
+
+  constructor(private accountService: AccountService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+   
+    const token = this.accountService.getToken();
+
+    if (token != null){
+      
+      request = request.clone({
+        headers: request.headers.set('Authorization', `Bearer ${token}`)
+        //   setHeaders: {
+          //     Authorization: `Bearer ${token}`
+          // }
+        })
+      }
+      
+      console.log(request);
+    return next.handle(request);
+  }
+}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {InterceptInterceptor } from './_Helper/intercept.interceptor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [  
+    { provide: HTTP_INTERCEPTORS, useClass: InterceptInterceptor, multi: true }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
