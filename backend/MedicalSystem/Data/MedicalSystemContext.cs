06f78779129d85c62d43f17d16eaec05f098bd10
Rehab Samy <rehabsamy18@gmail.com>
2022-07-14 03:18:13 +0200

    Patient can edit his appointments with doctors

--- backend/MedicalSystem/Data/MedicalSystemContext.cs
+++ backend/MedicalSystem/Data/MedicalSystemContext.cs
@@ -32,8 +32,7 @@ namespace MedicalSystem.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=medical-system-server.database.windows.net;Initial Catalog=MedicalSystem;Persist Security Info=True;User ID=Team;Password=Password123");
-            }
+optionsBuilder.UseSqlServer("Data Source=medical-system-server.database.windows.net;Initial Catalog=MedicalSystem;Persist Security Info=True;User ID=Team;Password=Password123");            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
