edbabdbabc16b383a2351e9dd6838d32faa4a5db
Fady Wafic <Fadywafic.123@gmail.com>
2022-07-16 13:47:04 +0200

    add background to admin component

--- Frontend/src/app/admin/admin.module.ts
+++ Frontend/src/app/admin/admin.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { DoctorRegisterationComponent } from './doctor-registeration/doctor-registeration.component';
 import { FormsModule } from '@angular/forms';
 import { FileUpadateOrDeleteComponent } from './file-upadate-or-delete/file-upadate-or-delete.component';
@@ -25,7 +24,6 @@ import { DisplayDoctorComponent } from './display-doctor/display-doctor.componen
     DoctorRegisterationComponent,
     FileUpadateOrDeleteComponent,
     SafePipe,
-    AdminHomeComponent,
     DoctorRegisterationComponent,
     AgeValDirective,
     DoctorScheduleComponent,
